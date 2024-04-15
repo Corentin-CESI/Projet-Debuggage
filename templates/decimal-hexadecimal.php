2578dd360cdc7891f29babcaf3213ae01000638a
RaspY737 <161023878+RaspY737@users.noreply.github.com>
2024-04-15 15:39:28 +0200

    bs - DEC/HEXA

--- templates/decimal-hexadecimal.php
+++ templates/decimal-hexadecimal.php
@@ -5,12 +5,12 @@
 ?>
 
 <section id="homepage" class="homepage">
-    <div class="container">
-        <div class="section-title">
+    <div class="container-fluid row">
+        <div class="section-title col-11 mx-auto">
             <h2>Convertisseur système décimal positif en binaire </h2>
         </div>
 
-        <div class="row">
+        <div class="col-11 mx-auto">
             <figure class="bg-light rounded p-3">
                 <blockquote cite="https://www.huxley.net/bnw/four.html">
                     <p>
@@ -23,72 +23,73 @@
             </figure>
         </div>
 
-        <div class="row">
-            <fieldset class="col-6 mt-4">
-                <legend>Convertisseur</legend>
-                <form action="" name="decimal-hexadecimal">
-                    <div class="form-group row">
-                        <div class="col-12">
-                            <label for="decimal">Décimal</label>
-                            <div class="input-group">
-                                <input id="decimal" name="decimal" type="number" min="0" class="form-control" required>
+        <div class="col-11 mx-auto">
+            <div class="container-fluid row">
+                <fieldset class="col-11 mt-4 mx-auto pb-3">
+                    <legend>Convertisseur</legend>
+                    <form action="" name="decimal-hexadecimal">
+                        <div class="form-group row g-2">
+                            <div class="col-12">
+                                <label for="decimal">Décimal</label>
+                                <div class="input-group">
+                                    <input id="decimal" name="decimal" type="number" min="0" class="form-control" required>
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="col-12">
-                            <label for="hex">Héxadécimal</label>
-                            <div class="input-group">
-                                <input id="hex" name="hex" type="text"  class="form-control" disabled>
+                            
+                            <div class="col-12">
+                                <label for="hex">Héxadécimal</label>
+                                <div class="input-group">
+                                    <input id="hex" name="hex" type="text"  class="form-control" disabled>
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="col-12">
-                            <label for="binary">Binaire</label>
-                            <div class="input-group">
-                                <input id="binary" name="binary" type="text" min="0" class="form-control" disabled>
+                            
+                            <div class="col-12">
+                                <label for="binary">Binaire</label>
+                                <div class="input-group">
+                                    <input id="binary" name="binary" type="text" min="0" class="form-control" disabled>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </form>
             </fieldset>
-            </div>
         </div>
+    </div>
 </section>
 
+<script type="text/javascript">
+    window.addEventListener('load', () => {
+        let forms = document.forms;
+        let decimal = forms['decimal-hexadecimal'].elements['decimal'];
 
-    <script type="text/javascript">
-        window.addEventListener('load', () => {
-            let forms = document.forms;
-            let decimal = forms['decimal-hexadecimal'].elements['decimal'];
+        function dec2Hex(dec) {
+            return Math.abs(dec).toString(16);
+        }
 
-            function dec2Hex(dec) {
-                return Math.abs(dec).toString(16);
+        function convertToBinary(x) {
+            let bin = 0;
+            let rem, i = 1, step = 1;
+            while (x !== 0) {
+                rem = x % 2;
+                console.log(
+                    `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
+                );
+                x = parseInt(x / 2);
+                bin = bin + rem * i;
+                i = i * 10;
             }
+            return bin;
+        }
 
-            function convertToBinary(x) {
-                let bin = 0;
-                let rem, i = 1, step = 1;
-                while (x !== 0) {
-                    rem = x % 2;
-                    console.log(
-                        `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
-                    );
-                    x = parseInt(x / 2);
-                    bin = bin + rem * i;
-                    i = i * 10;
-                }
-                return bin;
-            }
+        decimal.addEventListener('input', () => {
+            let hex = forms['decimal-hexadecimal'].elements['hex'];
+            let binary = forms['decimal-hexadecimal'].elements['binary'];
 
-            decimal.addEventListener('input', () => {
-                let hex = forms['decimal-hexadecimal'].elements['hex'];
-                let binary = forms['decimal-hexadecimal'].elements['binary'];
-
-                hex.value = dec2Hex(decimal.value);
-                binary.value = convertToBinary(decimal.value);
-            });
-
+            hex.value = dec2Hex(decimal.value);
+            binary.value = convertToBinary(decimal.value);
         });
-    </script>
+
+    });
+</script>
 
 <?php template('footer');
