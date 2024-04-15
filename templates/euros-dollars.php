f2a948548d59d7af615810af27f1d6f45cfcbee7
RaspY737 <161023878+RaspY737@users.noreply.github.com>
2024-04-15 15:53:16 +0200

    bs - MONEY

--- templates/euros-dollars.php
+++ templates/euros-dollars.php
@@ -6,86 +6,88 @@ template('header', array(
 
     <!-- ======= About Section ======= -->
     <section id="homepage" class="homepage">
-        <div class="container">
-            <div class="section-title">
+        <div class="container-fluid row">
+            <div class="section-title col-11 mx-auto">
                 <h2>Convertisseur de devise</h2>
             </div>
 
-            <div class="row">
-
-                <fieldset class="col-12 mt-4">
-                    <legend>Euro vers dollar américain</legend>
-                    <form action="" method="post" name="euros-dollars">
-                        <div class="form-group row">
-                            <div class="col">
-                                <label for="EUR" aria-hidden="true" hidden>Euros</label>
-                                <div class="input-group">
-                                    <input id="EUR" name="EUR" type="text" class="form-control" required>
-                                    <div class="input-group-append">
-                                        <div class="input-group-text">€</div>
+            <div class="col-11 mx-auto">
+                <div class="container-fluid row">
+                    <fieldset class="col-11 mx-auto mt-4 pb-3 pt-3">
+                        <legend>Euro vers dollar américain</legend>
+                        <form action="" method="post" name="euros-dollars">
+                            <div class="form-group row">
+                                <div class="col-5">
+                                    <label for="EUR" aria-hidden="true" hidden>Euros</label>
+                                    <div class="input-group">
+                                        <input id="EUR" name="EUR" type="text" class="form-control" required>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">€</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="d-inline-flex align-items-center ">
-                                <span class="ver">vaut actuellement</span>
-                            </div>
+                                <div class="d-inline-flex align-items-center col-2">
+                                    <span class="ver">vaut actuellement</span>
+                                </div>
 
-                            <div class="col">
-                                <label for="USD" aria-hidden="true" hidden>Dollars</label>
-                                <div class="input-group">
-                                    <input id="USD" name="USD" type="text" class="form-control" disabled>
-                                    <div class="input-group-append">
-                                        <div class="input-group-text">$</div>
+                                <div class="col-5">
+                                    <label for="USD" aria-hidden="true" hidden>Dollars</label>
+                                    <div class="input-group">
+                                        <input id="USD" name="USD" type="text" class="form-control" disabled>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">$</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-2">
-                                <button name="submit" type="submit" class="btn btn-primary btn-block">Calculer</button>
-                            </div>
+                                <div class="col-2 ms-auto mt-2">
+                                    <button name="submit" type="submit" class="btn btn-primary btn-block col-12">Calculer</button>
+                                </div>
 
-                            <!--https://fr.calcuworld.com/calculs-mathematiques/calculatrice-pourcentage/-->
-                        </div>
-                    </form>
-                </fieldset>
+                                <!--https://fr.calcuworld.com/calculs-mathematiques/calculatrice-pourcentage/-->
+                            </div>
+                        </form>
+                    </fieldset>
 
-                <fieldset class="col-12 mt-4">
-                    <legend>Dollar américain vers euro</legend>
-                    <form action="" method="post" name="euros-dollars">
-                        <div class="form-group row">
-                            <div class="col">
-                                <label for="USD" aria-hidden="true" hidden>Dollars</label>
-                                <div class="input-group">
-                                    <input id="USD" name="USD" type="text" class="form-control" required>
-                                    <div class="input-group-append">
-                                        <div class="input-group-text">$</div>
+                    <fieldset class="col-11 mx-auto mt-4 pb-3 pt-3">
+                        <legend>Dollar américain vers euro</legend>
+                        <form action="" method="post" name="euros-dollars">
+                            <div class="form-group row">
+                                <div class="col-5">
+                                    <label for="USD" aria-hidden="true" hidden>Dollars</label>
+                                    <div class="input-group">
+                                        <input id="USD" name="USD" type="text" class="form-control" required>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">$</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="d-inline-flex align-items-center ">
-                                <span class="ver">vaut actuellement</span>
-                            </div>
+                                <div class="d-inline-flex align-items-center col-2">
+                                    <span class="ver">vaut actuellement</span>
+                                </div>
 
-                            <div class="col">
-                                <label for="EUR" aria-hidden="true" hidden>Euros</label>
-                                <div class="input-group">
-                                    <input id="EUR" name="EUR" type="text" class="form-control" disabled>
-                                    <div class="input-group-append">
-                                        <div class="input-group-text">€</div>
+                                <div class="col-5">
+                                    <label for="EUR" aria-hidden="true" hidden>Euros</label>
+                                    <div class="input-group">
+                                        <input id="EUR" name="EUR" type="text" class="form-control" disabled>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">€</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="col-2">
-                                <button name="submit" type="submit" class="btn btn-primary btn-block">Calculer</button>
+                                <div class="col-2 ms-auto mt-2">
+                                    <button name="submit" type="submit" class="btn btn-primary btn-block col-12">Calculer</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </fieldset>
+                        </form>
+                    </fieldset>
                 </div>
             </div>
-    </section><!-- End Home Section -->
+        </div>
+    </section>
+    <!-- End Home Section -->
 
 
     <script type="text/javascript">
