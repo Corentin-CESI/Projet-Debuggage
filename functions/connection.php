466ed68ee569126ab65ffde4875b849f7def143b
RaspY737 <161023878+RaspY737@users.noreply.github.com>
2024-04-22 10:37:39 +0200

    Mise en place du fichier .env Faire la commande : composer require vlucas/phpdotenv DANS le dossier de debogage

--- functions/connection.php
+++ functions/connection.php
@@ -1,9 +1,33 @@
 <?php
+// $db = include ROOT_PATH . 'config.php';
+// // Pourquoi pas utiliser un .ENV ?
+// define('DB_HOST', $db['host']);
+// define('DB_USER', $db['user']);
+// define('DB_PASSWORD', $db['password']);
+// define('DB_NAME', $db['name']);
+// define('DB_PORT', $db['port']);
 
-$db = include ROOT_PATH . 'config.php';
-// Pourquoi pas utiliser un .ENV ?
-define('DB_HOST', $db['host']);
-define('DB_USER', $db['user']);
-define('DB_PASSWORD', $db['password']);
-define('DB_NAME', $db['name']);
-define('DB_PORT', $db['port']);
+
+// namespace App\Controller;
+
+// class DotEnvEnvironmentTest {
+//    public function load($path): void {
+//        $lines = file($path . '/.env');
+//        foreach ($lines as $line) {
+//            [$key, $value] = explode('=', $line, 2);
+//            $key = trim($key);
+//            $value = trim($value);
+
+//            putenv(sprintf('%s=%s', $key, $value));
+//            $_ENV[$key] = $value;
+//            $_SERVER[$key] = $value;
+//         }   
+//     }
+// }
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
--- functions/database.php
+++ functions/database.php
@@ -7,8 +7,8 @@
  *
  * return mixed
  */
-function run_query(string $query) {
-    $connection  = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+function run_query(string $query) {    
+    $connection  = @mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
     if (mysqli_connect_errno()) {
         throw new Exception("Database connection failed: " . mysqli_connect_error());
     }
